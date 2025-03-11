bd1e1e45df96eed4194b7d661b65f4f7b8a8f9bc
bwire <ahmedbwire17@gmail.com>
2025-03-11 06:17:48 +0300

    Adds js arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// 1. Multiply
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // 62
+
+// 2. Random
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+// 3. Mod
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // 4
+
+// 4. Max
+const max = Math.max(1, 5, 10, 15, 20); // 20
